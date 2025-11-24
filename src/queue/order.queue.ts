48b380f1c62b30187a46ffc0c0d22f1fb2961100
Rushil Shrivastava <rushil@Rushils-Mac-mini.local>
2025-11-25 03:09:29 +0530

    Added tls in redis for Railway production

--- src/queue/order.queue.ts
+++ src/queue/order.queue.ts
@@ -2,6 +2,7 @@ import { Queue } from "bullmq";
 import IORedis from "ioredis";
 
 const connection = new IORedis(process.env.REDIS_URL || "redis://localhost:6379", {
+  tls: {},
   maxRetriesPerRequest: null
 });
 
